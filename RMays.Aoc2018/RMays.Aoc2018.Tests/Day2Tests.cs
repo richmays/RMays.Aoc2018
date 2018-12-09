909638aaf865939c545a4029519a885b728fbc95
rmays <richmays+git@gmail.com>
2018-12-08 22:17:54 -0500

    D2 complete

--- RMays.Aoc2018/RMays.Aoc2018.Tests/Day2Tests.cs
+++ RMays.Aoc2018/RMays.Aoc2018.Tests/Day2Tests.cs
@@ -11,36 +11,36 @@ namespace RMays.Aoc2018.Tests
     public class Day2Tests
     {
         [Test]
-        [TestCase("1, 2, 3", 123)]
+        [TestCase("abcdef, bababc, abbcde, abcccd, aabcdd, abcdee, ababab", 12)]
         public void PartATests(string input, int expectedOutput)
         {
-            var day = new Day0();
+            var day = new Day2();
             var result = day.SolveA(input);
             Assert.AreEqual(expectedOutput, result);
         }
 
         [Test]
-        [TestCase("4, 5, 6", 456)]
+        [TestCase("abcde, fghij, klmno, pqrst, fguij, axcye, wvxyz", "fgij")]
 
-        public void PartBTests(string input, int expectedOutput)
+        public void PartBTests(string input, string expectedOutput)
         {
-            var day = new Day0();
+            var day = new Day2();
             var result = day.SolveB(input);
             Assert.AreEqual(expectedOutput, result);
         }
 
         [Test]
-        public void DoItA() // ?
+        public void DoItA() // 5390
         {
-            var day = new Day0();
-            Console.WriteLine(day.SolveA(InputData.Day0));
+            var day = new Day2();
+            Console.WriteLine(day.SolveA(InputData.Day2));
         }
 
         [Test]
-        public void DoItB() // ?
+        public void DoItB() // nvosmkcdtdbfhyxsphzgraljq
         {
-            var day = new Day0();
-            Console.WriteLine(day.SolveB(InputData.Day0));
+            var day = new Day2();
+            Console.WriteLine(day.SolveB(InputData.Day2));
         }
     }
 }
