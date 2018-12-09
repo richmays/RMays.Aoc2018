169e61ef088274e4a9b020a6d35c33d33d9d27c8
rmays <richmays+git@gmail.com>
2018-12-08 23:46:12 -0500

    D4 done

--- RMays.Aoc2018/RMays.Aoc2018/Parser.cs
+++ RMays.Aoc2018/RMays.Aoc2018/Parser.cs
@@ -8,6 +8,11 @@ namespace RMays.Aoc2018
 {
     public class Parser
     {
+        public static List<string> Tokenize(string input, char splitter)
+        {
+            return input.Split(splitter).Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).ToList();
+        }
+
         public static List<string> Tokenize(string input)
         {
             return input.Split(new char[] { ',', '\r', '\n' }).Where(x => !string.IsNullOrWhiteSpace(x)).Select(x => x.Trim()).ToList();
