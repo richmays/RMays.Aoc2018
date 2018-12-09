f773c3c83ad85419b0a13a94241837ca3a15e47d
rmays <richmays+git@gmail.com>
2018-12-08 20:59:05 -0500

    Getting started

---
+++ RMays.Aoc2018/RMays.Aoc2018.Tests/Day1Tests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2018.Tests
+{
+    [TestFixture]
+    public class Day1Tests
+    {
+        [Test]
+        public void MyFirstTest()
+        {
+            Assert.IsTrue(1 == 1);
+        }
+    }
+}
--- RMays.Aoc2018/RMays.Aoc2018/Day1.cs
+++ RMays.Aoc2018/RMays.Aoc2018/Day1.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace RMays.Aoc2018
 {
-    public class Class1
+    public class Day1
     {
     }
 }
