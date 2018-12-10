a3a26c16060a52f93fe476ee2b50d01e33c52eb2
rmays <richmays+git@gmail.com>
2018-12-09 21:32:56 -0500

    No idea.  :(

---
+++ RMays.Aoc2018/RMays.Aoc2018/Day6.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2018
+{
+    public class Day6
+    {
+        public long SolveA(string input)
+        {
+            var myList = Parser.TokenizeLines(input);
+
+
+
+
+
+            return 0;
+        }
+
+        public long SolveB(string input)
+        {
+            var myList = Parser.Tokenize(input);
+      
+
+            return 0;
+        }
+    }
+}
