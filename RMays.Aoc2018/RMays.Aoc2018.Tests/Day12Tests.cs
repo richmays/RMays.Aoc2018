d65b3da223ac7a47326bead65f44fd682a369e1a
rmays <richmays+git@gmail.com>
2019-01-02 22:58:24 -0500

    D12, first star

---
+++ RMays.Aoc2018/RMays.Aoc2018.Tests/Day12Tests.cs
@@ -0,0 +1,61 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2018.Tests
+{
+    [TestFixture]
+    public class Day12Tests
+    {
+        [Test]
+        [TestCase(@"initial state: #..#.#..##......###...###
+
+...## => #
+..#.. => #
+.#... => #
+.#.#. => #
+.#.## => #
+.##.. => #
+.#### => #
+#.#.# => #
+#.### => #
+##.#. => #
+##.## => #
+###.. => #
+###.# => #
+####. => #", 325)]
+        public void PartATests(string input, int expectedOutput)
+        {
+            var day = new Day12();
+            var result = day.SolveA(input, 20);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void PartATestsRealData()
+        {
+            var expectedOutput = 4386;
+            var day = new Day12();
+            var result = day.SolveA(InputData.Day12, 20);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void DoItA() // 4386
+        {
+            var day = new Day12();
+            Console.WriteLine(day.SolveA(InputData.Day12, 20));
+        }
+
+        [Test]
+        public void DoItB() // ?
+        {
+            var day = new Day12();
+            Console.WriteLine(day.SolveA(InputData.Day12, 5000));
+            //Console.WriteLine(day.SolveA(InputData.Day12, 50000000000));
+        }
+    }
+}
---
+++ RMays.Aoc2018/RMays.Aoc2018/Day12.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMays.Aoc2018
+{
+    public class Pots
+    {
+        private HashSet<int> PotsSet;
+        private int LowPlant;
+        private int HighPlant;
+        public Pots()
+        {
+            PotsSet = new HashSet<int>();
+            LowPlant = 0;
+            HighPlant = 0;
+        }
+
+        /// <summary>
+        /// Returns whether or not there's a plant in the pot.
+        /// </summary>
+        /// <param name="index"></param>
+        /// <returns></returns>
+        public bool GetPot(int index)
+        {
+            return PotsSet.Contains(index);
+        }
+
+        public void SetPot(int index, bool hasPlant)
+        {
+            PotsSet.Remove(index);
+            if (hasPlant)
+            {
+                PotsSet.Add(index);
+            }
+        }
+
+        private void CalcHighLow()
+        {
+            LowPlant = PotsSet.Min();
+            HighPlant = PotsSet.Max();
+        }
+
+        public void Go(List<string> rules, long generations)
+        {
+            int prevSum = 0;
+            for (var gen = 0; gen < generations; gen++)
+            {
+                CalcHighLow();
+                var NewPotsSet = new HashSet<int>();
+                for (int index = LowPlant - 1; index <= HighPlant + 1; index++)
+                {
+                    string orientation = "";
+                    for (int currPlant = index - 2; currPlant <= index + 2; currPlant++)
+                    {
+                        orientation += (this.GetPot(currPlant) ? '#' : '.');
+                    }
+
+                    if (rules.Contains(orientation))
+                    {
+                        NewPotsSet.Add(index);
+                    }
+                }
+
+                PotsSet.Clear();
+                foreach (var pot in NewPotsSet)
+                {
+                    PotsSet.Add(pot);
+                }
+
+                /*
+                var plantSum = this.PlantSum();
+                Console.WriteLine($"{gen}: {plantSum} ({plantSum - prevSum})");
+                prevSum = plantSum;
+                */
+            }
+        }
+
+        public int PlantSum()
+        {
+            return PotsSet.Sum(x => x);
+        }
+
+        public override string ToString()
+        {
+            CalcHighLow();
+            var toReturn = $"{LowPlant}: ";
+            for(int i = this.LowPlant; i <= this.HighPlant; i++)
+            {
+                toReturn += (GetPot(i) ? '#' : '.');
+            }
+            return toReturn;
+            //return "?";
+        }
+    }
+
+    public class Day12
+    {
+        public long SolveA(string input, long generations)
+        {
+            // initial state: #..#.#..##......###...###
+            var intialPots = Parser.TokenizeLines(input)[0].Split(' ')[2];
+
+            var myPots = new Pots();
+            int potId = 0;
+            foreach(var pot in intialPots.ToCharArray())
+            {
+                if (pot == '#')
+                {
+                    myPots.SetPot(potId, true);
+                }
+                potId++;
+            }
+
+            // Now read the rules.
+
+            // ...## => #
+            var rulesText = Parser.TokenizeLines(input);
+            var rules = new List<string>();
+            foreach(var line in rulesText)
+            {
+                var splitLine = line.Split(' ').ToList();
+                if (splitLine[1] != "=>") continue;
+                if (splitLine[2] == "#")
+                {
+                    rules.Add(splitLine[0]);
+                }
+            }
+
+            myPots.Go(rules, generations);
+            return myPots.PlantSum();
+        }
+    }
+}
